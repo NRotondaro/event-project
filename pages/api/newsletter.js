1ad56662d8daafd78877af1dbee12491a0870c19
NRotondaro <niksonrotondaro1@gmail.com>
2023-08-31 20:42:35 -0300

    feat: insert comment in the database

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -7,7 +9,16 @@ function handler(req, res) {
       return;
     }
 
-    console.log(userEmail);
+    const client = await MongoClient.connect(
+      'mongodb+srv://noskin:noskin34@noskin.2tzwqbk.mongodb.net/events'
+    );
+
+    const db = client.db();
+
+    await db.collection('newsletter').insertOne({ email: userEmail });
+
+    client.close();
+
     res.status(201).json({ message: 'Signed up!' });
   }
 }
