b18e0e713259b2593367a7861fb76f59a7b343ea
NRotondaro <niksonrotondaro1@gmail.com>
2023-08-31 19:08:56 -0300

    feat: add comments & newsletter

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,10 +1,11 @@
 import { Fragment } from 'react';
+import Head from 'next/head';
 
 import { getEventById, getFeaturedEvents } from '../../helpers/api-utils';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
-import Head from 'next/head';
+import Comments from '../../components/input/comments';
 
 function EventDetailPage(props) {
   const event = props.selectedEvent;
@@ -33,6 +34,7 @@ function EventDetailPage(props) {
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
+      <Comments eventId={event.id} />
     </Fragment>
   );
 }
